24aacfda72b1f589c0916527f2d22e9bf94f3204
isadora-vital <isadora.vital@usp.br>
2024-12-06 20:44:07 -0300

    Modificações 1.0

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 #include <zephyr/toolchain.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
@@ -16,51 +17,74 @@
 #define PRIORITY 0
 #define limiteFIFO 1000
 
-K_FIFO_DEFINE(entradaFIFO);
-K_FIFO_DEFINE(saidaFIFO);
+K_FIFO_DEFINE(fifo_in);
+K_FIFO_DEFINE(fifo_out);
 
 int elementosFIFO_IN = 0;
 int elementosFIFO_OUT = 0;
 
-struct data_item_t {
-    void *fifo_reserved;   /* 1st word reserved for use by FIFO */
-    char value[250];
+struct pacote {
+    uint8_t lixo;
+    uint8_t syn;
+    uint8_t stx;
+    uint8_t id;
+    uint8_t N;
+    char dado[7];
+    uint8_t etx;
 };
 
-struct data_item_t tx_data;
-string tecladoIn;
+char tecladoIn[7];
+//---------------------------------------------------------------------------
+extern void enviaPacote(struct k_timer *timer_id){
+    //envio de todos os dados
 
+    //lixo: 0110 0110
+    //SYN: 0001 0110
+    //STX: 0000 0010
+    //ID: 11011 e N: definido pela entrada
+    //DADO: inserido pelo usuário
+    //ETX: 0000 0011
+
+    static struct pacote segmento;
+    segmento.lixo = 0b01100110;
+    segmento.syn = 0b00010110;
+    segmento.stx = 0b00000010;
+    segmento.id = 0b11011000 & 0xF8; //5 bits iniciais
+
+    printk("\nMensagem de ate 7 caracteres:\n");
+    scanf("%7s", tecladoIn); //confere se string tem <= 7 caracteres
+
+    segmento.N = strlen(tecladoIn) & 0x07; // 3 bits finais
+    strcpy(segmento.dado, tecladoIn); //manda info para segmento.dado
+    segmento.etx = 0b00000011;
+
+    k_fifo_put(&entradaFIFO, &tx_data); //envia info p/fifo
+    elementosFIFO_IN++;
+}
+
+K_TIMER_DEFINE(timerTX, pacote, NULL);
+//---------------------------------------------------------------------------
 void IN(void){
     /* teclado */
-    while(1){
-        scanf("%s", &tecladoIn);
-        //confere se string tem <= 7 caracteres
-        if(telcadoIn)
-        strcpy(tx_data.value, tecladoIn); //manda info para tx_data
-
-        //config FIFO 
-
-
-        k_fifo_put(&entradaFIFO, &tx_data); //envia info p/fifo
-        elementosFIFO_IN++;
-    }
+    k_timer_start(&timerTX, K_MSEC(10), K_MSEC(10));
 }
 
 void TX(void){
     /* transmissão de informação */
-    while(1){
-        if(elementosFIFO >= limiteFIFO){
-            while(elementosFIFO > (limiteFIFO/2)){
-                //FIFO cheia
-                k_msleep(SLEEP_TIME_MS);
-            }
-        } else{
-            strcpy(tx1_data.value, "oioi!"); //manda info para tx1_data
-            k_fifo_put(&my_fifo, &tx1_data); //envia info p/fifo
-            elementosFIFO++;
+    //timer!!! 10ms
+    //Ji: tirei o while
+    if(elementosFIFO >= limiteFIFO){
+        while(elementosFIFO > (limiteFIFO/2)){
+            //FIFO cheia
+            k_msleep(SLEEP_TIME_MS);
         }
-        k_msleep(SLEEP_TIME_MS);
+    } else{
+        strcpy(tx1_data.value, "oioi!"); //manda info para tx1_data
+        k_fifo_put(&my_fifo, &tx1_data); //envia info p/fifo
+        elementosFIFO++;
     }
+    k_msleep(SLEEP_TIME_MS);
+
 }
 
 void RX(void){
